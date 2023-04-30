78eac6ba411496bd12d5d704c3b1d33aaa7a3a6d
Srikrishna <krishna333sp@gmail.com>
2023-04-30 11:43:46 +0530

    link

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,13 +1,14 @@
 import "./header.css";
 import Logo from "../assests/instaclone.svg";
 import Camera from "../assests/camera.gif";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <>
             <div className="nav">
                 <a href="/"> <img src={Logo} alt="insta-logo" id="logo" title="Go to Home Page"></img></a>
-                <a href="/postform"><img src={Camera} alt="camera" id="camera" title="Post now your memories!"></img></a>
+                <Link to="/postform"><img src={Camera} alt="camera" id="camera" title="Post now your memories!"></img></Link>
             </div>
         </>
     )
